8485a86d11a8d644af349a882f0a11c9ad613428
HarryDhw <95197041+HarryDhw@users.noreply.github.com>
2022-01-06 21:58:26 +0800

    2022/1/6 photo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let bt = document.getElementById('light');
 let bg = document.querySelector('body');
 const cg = () => {
     if(bg.style.color === 'snow') {
-        bg.style.backgroundColor = 'lightblue';
+        bg.style.backgroundColor = 'white';
         bg.style.color = 'black';
     }else {
         bg.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
